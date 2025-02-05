62704bb3624212071c67806c918e82c82762a1a8
ludo <1940381@carrefour.cegepvicto.ca>
2025-02-05 13:40:43 -0500

    ajout du fichier .env

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import salutationsRouter from './src/routes/salutations.route.js';
 import { salutations,Ajoutersalut } from './src/models/salutations.model.js';
 // Créer une application express
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.use('/', salutationsRouter);
 app.use(express.json());
 app.get('/', (req, res) => {
